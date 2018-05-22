1b1e7eb9b9840631aa020041448c7e716fe37a3c
Henrique Foletto <henriquevidalfoletto@gmail.com>
2018-05-22 10:22:38 -0300

    Removendo pasta .idea

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class BooksApp extends React.Component {
         ) : (
           <div className="list-books">
             <div className="list-books-title">
-              <h1>MyReads 0.1</h1>
+              <h1>MyReads 0.2</h1>
             </div>
             <div className="list-books-content">
               <div>
