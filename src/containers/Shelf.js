2c4dcaaf94823f8a9c217faa0b7d54ab9015ee5f
Henrique Foletto <henriquevidalfoletto@gmail.com>
2018-05-23 03:58:36 -0300

    Componente Book e container Shelf criados

--- src/containers/Shelf.js
+++ src/containers/Shelf.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Book from '../components/Book'
 
 class Shelf extends Component {
-  static propTypes = {}
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  }
 
   state = {}
 
   render() {
     return (
-      <div>Shelf</div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book, index) => (
+              <Book key={index} title={book.title} author={book.author} img={book.img}/>
+            ))}
+          </ol>
+        </div>
+      </div>
     )
   }
 }
