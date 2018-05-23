a1f78e22695c75d65f576050a3c7a4b22a4ab859
Henrique Foletto <henriquevidalfoletto@gmail.com>
2018-05-23 03:57:20 -0300

    Componente Book e container Shelf criados

---
+++ src/components/Book.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+  static propTypes = {}
+
+  state = {}
+
+  render() {
+    return (
+      <div>Book</div>
+    )
+  }
+}
+
+export default Book
---
+++ src/containers/Shelf.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Shelf extends Component {
+  static propTypes = {}
+
+  state = {}
+
+  render() {
+    return (
+      <div>Shelf</div>
+    )
+  }
+}
+
+export default Shelf
